bf4f73e0ae2679bcc1bcd9dfaec5f7bfaeb8a2f1
AO Industries, Inc. <support@aoindustries.com>
2016-08-10 16:07:39 -0500

    Diagrams are now elements.

---
+++ src/main/java/com/semanticcms/dia/model/Dia.java
@@ -0,0 +1,117 @@
+/*
+ * semanticcms-dia-model - Java API for embedding Dia-based diagrams in web pages.
+ * Copyright (C) 2013, 2014, 2015, 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-dia-model.
+ *
+ * semanticcms-dia-model is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-dia-model is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-dia-model.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.dia.model;
+
+import com.semanticcms.core.model.Element;
+
+/**
+ * TODO: Find way to register a view dynamically on stock docs screens?
+ */
+public class Dia extends Element {
+
+	public static final String EXTENSION = "dia";
+	public static final String DOT_EXTENSION = "." + EXTENSION;
+
+	private String label;
+	private String book;
+	private String path;
+	private int width;
+	private int height;
+
+	@Override
+	public Dia freeze() {
+		super.freeze();
+		return this;
+	}
+
+	/**
+	 * If not set, defaults to the last path segment of path, with any ".dia" extension stripped.
+	 */
+	@Override
+	public String getLabel() {
+		if(label != null) return label;
+		if(path != null) {
+			String filename = path.substring(path.lastIndexOf('/') + 1);
+			if(filename.endsWith(DOT_EXTENSION)) filename = filename.substring(0, filename.length() - DOT_EXTENSION.length());
+			if(filename.isEmpty()) throw new IllegalArgumentException("Invalid filename for diagram: " + path);
+			return filename;
+		}
+		throw new IllegalStateException("Cannot get label, neither label nor path set");
+	}
+
+	public void setLabel(String label) {
+		checkNotFrozen();
+		this.label = label==null || label.isEmpty() ? null : label;
+	}
+
+	public String getBook() {
+		return book;
+	}
+
+	public void setBook(String book) {
+		checkNotFrozen();
+		this.book = book==null || book.isEmpty() ? null : book;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		checkNotFrozen();
+		this.path = path==null || path.isEmpty() ? null : path;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		checkNotFrozen();
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		checkNotFrozen();
+		this.height = height;
+	}
+
+	@Override
+	public String getListItemCssClass() {
+		return "semanticcms-dia-list-item-dia";
+	}
+
+	@Override
+	protected String getDefaultIdPrefix() {
+		return "dia";
+	}
+
+	@Override
+	public String getLinkCssClass() {
+		return "semanticcms-dia-dia-link";
+	}
+}
