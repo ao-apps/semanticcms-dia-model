a48dd254371644ace4ad142559acbae794e885c6
AO Industries, Inc <support@aoindustries.com>
2016-08-10 10:48:23 -0500

    Matched to 6b240bf8fd6d9ce06c332b44673afce04bdffd2b

--- src/main/java/com/semanticcms/dia/model/DiaExport.java
+++ src/main/java/com/semanticcms/dia/model/DiaExport.java
@@ -1,26 +1,26 @@
 /*
- * ao-web-dia - Java API for embedding Dia-based diagrams in web pages.
+ * semanticcms-dia-model - Java API for embedding Dia-based diagrams in web pages.
  * Copyright (C) 2013, 2014, 2015, 2016  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
  *
- * This file is part of ao-web-dia.
+ * This file is part of semanticcms-dia-model.
  *
- * ao-web-dia is free software: you can redistribute it and/or modify
+ * semanticcms-dia-model is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ao-web-dia is distributed in the hope that it will be useful,
+ * semanticcms-dia-model is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-web-dia.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-dia-model.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.aoindustries.web.dia;
+package com.semanticcms.dia.model;
 
 import java.io.File;
 
