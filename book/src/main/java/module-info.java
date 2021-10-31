1aff41c7944510e783debb625d69ad89749cf1a2
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:47:20 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/module-info.java
+++ book/src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-dia-model.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-dia-model.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.dia.model.book {
 	exports com.semanticcms.dia.model.book;
