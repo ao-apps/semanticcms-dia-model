de01319a8c537586ebf24f9565cd42d63933345e
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:45 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/dia/model/Dia.java
+++ src/main/java/com/semanticcms/dia/model/Dia.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-dia-model - Java API for embedding Dia-based diagrams in web pages.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-dia-model.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.dia.model;
 
 import static com.aoapps.lang.Strings.nullIfEmpty;
